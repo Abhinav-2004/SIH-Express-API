b4b3e0969e265a2eb395a2841a65ce36ac53586a
Abhinav Singh <kittusinghranchi@gmail.com>
2023-11-17 13:39:09 +0530

    backend setup

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 //backend will run on port 8080
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(
+    cors({
+      origin: ['http://localhost:3000', 'http://localhost:3001'],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      credentials: true,
+    })
+  );
+const PORT=8080;
+console.log('Backend running')
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
